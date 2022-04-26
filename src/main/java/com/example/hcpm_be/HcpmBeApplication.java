504a6f8338f6dae8adeaeab382c0ed896a98d152
ThaiVu54 <thai01654369440@gmail.com>
2022-04-26 15:12:28 +0700

    fix swagger

--- src/main/java/com/example/hcpm_be/HcpmBeApplication.java
+++ src/main/java/com/example/hcpm_be/HcpmBeApplication.java
@@ -2,8 +2,13 @@ package com.example.hcpm_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @SpringBootApplication
+//@EnableSwagger2
+@EnableWebMvc
 public class HcpmBeApplication {
 
     public static void main(String[] args) {
@@ -11,3 +16,4 @@ public class HcpmBeApplication {
     }
 
 }
+
---
+++ src/main/java/com/example/hcpm_be/config/ExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.hcpm_be.config;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+@EnableWebMvc
+public class ExceptionHandler extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
